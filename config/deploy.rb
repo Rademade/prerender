9bfc4dfd2d8884e6d2d3ed7fcee9133c40db9a6c
Vladislav Melanitskiy <vlad.melanitski@gmail.com>
2015-08-12 18:15:38 +0300

    Fix deploy rules Add mongo cache

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,9 +5,6 @@ set :application, 'Prerender'
 set :scm, :git
 set :repo_url, 'git@github.com:Rademade/prerender.git'
 
-set :rvm_type,          :system
-set :rvm_ruby_version,  'ruby-2.1.5@prerender'
-
 set :linked_dirs, %w{bower_components node_modules}
 set :keep_releases, 2
 
@@ -25,7 +22,7 @@ namespace :deploy do
     on roles(:web) do
       within current_path do
         execute "kill $(ps aux | grep 'Prerender' | awk '{print $2}')"
-        execute "cd #{current_path} && APP=Prerender && PORT=4554 node_modules/.bin/forever start node_modules/prerender/server.js >> /dev/null 2>&1"
+        execute "cd #{current_path} && APP=Prerender && PORT=4554 node_modules/.bin/forever start server.js >> /dev/null 2>&1"
       end
     end
   end
--- config/deploy/rademade.rb
+++ config/deploy/rademade.rb
@@ -1,3 +1,6 @@
 set :deploy_to, "/var/www/prerender"
 
+set :rvm_type,          :system
+set :rvm_ruby_version,  'ruby-2.1.5@prerender'
+
 server 'rademade.com', user: 'deploy', roles: %w{web app}
