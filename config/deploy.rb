37c29ca9477410e0fe61d1f9b43dc51972c1bb57
Vladislav Melanitskiy <vlad.melanitski@gmail.com>
2015-08-12 14:46:26 +0300

    Update deploy settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ namespace :deploy do
     on roles(:web) do
       within current_path do
         execute "kill $(ps aux | grep 'Prerender' | awk '{print $2}')"
-        execute "cd #{current_path} && APP=Prerender && PORT=4554 node_modules/.bin/forever start server.js >> /dev/null 2>&1"
+        execute "cd #{current_path} && APP=Prerender && PORT=4554 node_modules/.bin/forever start node_modules/prerender/server.js >> /dev/null 2>&1"
       end
     end
   end
