a1f5df2deae47566ac6f354b7d2058fe1eccd51f
Yuri Kanivetsky <yuri.kanivetsky@gmail.com>
2018-02-06 00:22:44 +0200

    Add "clear prerender cache" task to schedule

--- config/deploy/rademade.rb
+++ config/deploy/rademade.rb
@@ -3,4 +3,4 @@ set :deploy_to, "/home/prerender/application-production"
 set :rvm_type,          :system
 set :rvm_ruby_version,  'ruby-2.3.0@prerender'
 
-server 'rademade.com', user: 'prerender', roles: %w{web app web}
+server 'rademade.com', user: 'prerender', roles: %w{web app db}
